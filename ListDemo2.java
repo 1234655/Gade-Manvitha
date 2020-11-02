4d310e7bb56e469174f28f3c902a0e813b012a94
1234655 <73590483+1234655@users.noreply.github.com>
2020-11-02 17:29:11 +0530

    Add files via upload

---
+++ ListDemo2.java
@@ -0,0 +1,30 @@
+package Employee;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Vector;
+
+public class ListDemo2 {
+	public static void main(String args[])
+	{
+		LinkedList<Integer> sh = new LinkedList<Integer>();
+		sh.add(12);
+		sh.add(13);
+		sh.add(14);
+		sh.add(15);
+		sh.add(16);
+		sh.add(17);
+		sh.add(18);
+		sh.add(19);
+		sh.add(20);
+		System.out.println(sh);
+		sh.add(3,92);//adds 92 at 3rd position
+		System.out.println(sh);
+		System.out.println(sh.get(5));//returns the element with 5th position
+		
+		}
+	}
+
+
