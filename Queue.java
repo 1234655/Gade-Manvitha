ae5ea1aeb8e309b138acd038d7a0434bbdcf3b9c
1234655 <73590483+1234655@users.noreply.github.com>
2020-11-02 17:34:11 +0530

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,26 @@
+package Employee;
+
+import java.util.PriorityQueue;
+
+public class Queue {
+	public static void main(String args[])
+	{
+		PriorityQueue<Integer> sh = new PriorityQueue<Integer>();
+		sh.add(12);
+		sh.add(13);
+		sh.add(14);
+		sh.add(15);
+		sh.add(16);
+		sh.add(17);
+		sh.add(18);
+		sh.add(19);
+		sh.add(20);
+		System.out.println(sh);
+		
+		
+		
+		
+		}
+	}
+
+
